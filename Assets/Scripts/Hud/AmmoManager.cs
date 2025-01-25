70b38c1e93b9cefa44853be3b694eef2f3e81d86
Albert <albertpinedabas@gmail.com>
2025-01-25 13:42:04 +0100

    no pasa nada chavales no pasa nada

--- Assets/Scripts/Hud/AmmoManager.cs
+++ Assets/Scripts/Hud/AmmoManager.cs
@@ -9,13 +9,13 @@ public class AmmoManager : MonoBehaviour
     public void AddAmmo() // Solo se puede de uno en uno
     {
         currentAmmo++;
-        UpdateScoreText();
+        
     }
 
     // Método para reiniciar la puntuación
     public void ResetAmmo()
     {
-        currentScore = 6;
+        currentAmmo = 6;
     }
 
     public void ShowAmmo()
