56a2a71bff0caaae74259a090fd571e7d6b6003f
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2025-01-25 13:50:14 +0100

    Bubble spawner with probabilities

--- Assets/Scripts/Bubbles/Spawner.cs
+++ Assets/Scripts/Bubbles/Spawner.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Collider2D))]
 public class Spawner : MonoBehaviour
 {
     private Collider2D spawnArea;
 
     public GameObject[] bubblesPrefabs;
+    public float[] spawnProbabilities;
 
     public float minSpawnDelay = 0.25f;
     public float maxSpawnDelay = 1f;
 
-   // public float maxLifetime = 5f;
-
     private void Awake()
     {
         spawnArea = GetComponent<Collider2D>();
+
+        if (spawnProbabilities.Length != bubblesPrefabs.Length)
+        {
+            Debug.LogError("The number of probabilities must match the number of bubble prefabs.");
+        }
     }
 
     private void OnEnable()
@@ -34,7 +38,7 @@ public class Spawner : MonoBehaviour
 
         while (enabled)
         {
-            GameObject prefab = bubblesPrefabs[Random.Range(0, bubblesPrefabs.Length)];
+            GameObject prefab = SelectBubbleBasedOnProbability();
 
             Vector3 position = new Vector3
             {
@@ -43,13 +47,34 @@ public class Spawner : MonoBehaviour
                 z = 0
             };
 
-            // Error: necesita rotation
             GameObject bubble = Instantiate(prefab, position, Quaternion.identity);
-        //    Destroy(bubble, maxLifetime);
-
 
             yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
         }
     }
 
+    private GameObject SelectBubbleBasedOnProbability()
+    {
+        float totalProbability = 0f;
+        foreach (float probability in spawnProbabilities)
+        {
+            totalProbability += probability;
+        }
+
+        float randomValue = Random.Range(0f, totalProbability);
+
+        float cumulativeProbability = 0f;
+
+        for (int i = 0; i < spawnProbabilities.Length; i++)
+        {
+            cumulativeProbability += spawnProbabilities[i];
+
+            if (randomValue <= cumulativeProbability)
+            {
+                return bubblesPrefabs[i];
+            }
+        }
+
+        return bubblesPrefabs[bubblesPrefabs.Length - 1];
+    }
 }
