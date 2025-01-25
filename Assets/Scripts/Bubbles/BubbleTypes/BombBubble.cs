d4aa609cd5ab2007b4748d8b04a790969e3adeb6
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2025-01-25 17:10:55 +0100

    namechange

--- Assets/Scripts/Bubbles/BubbleTypes/BombBubble.cs
+++ Assets/Scripts/Bubbles/BubbleTypes/BombBubble.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class BoomBubble : Bubble
 {
+    public GameOverScreen GameOverScreen;
+
     protected override void onClick()
     {
         Debug.Log("¡Boom! Explosive Bubble clicked!");
         Destroy(gameObject);
+        GameOverScreen.BeginGameOver(0);
     }
 }
