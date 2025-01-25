9f8dc5f2325a8777df9b5c9b8697654556a17799
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2025-01-25 23:24:55 +0100

    Update GameOverManager.cs

--- Assets/Scripts/Hud/GameOverManager.cs
+++ Assets/Scripts/Hud/GameOverManager.cs
@@ -28,6 +28,6 @@ public class GameOverManager : MonoBehaviour
 
     public void MainMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 }
