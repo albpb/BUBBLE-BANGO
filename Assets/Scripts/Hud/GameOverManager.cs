764a551d7d2919eb6e625dbbb2fcfadecd6c9435
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2025-01-26 01:19:00 +0100

    Ammo fix

--- Assets/Scripts/Hud/GameOverManager.cs
+++ Assets/Scripts/Hud/GameOverManager.cs
@@ -1,24 +1,39 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameOverManager : MonoBehaviour
 {
     public Text pointsText;
-
     public Text loseReason;
-
     public GameObject gameOverScreen;
-
     public ScoreManager scoreManager;
-
     public bool gameOver;
+
+    private AmmoManager ammoManager;
+
+    private void Awake()
+    {
+        ammoManager = FindAnyObjectByType<AmmoManager>();
+    }
+
     public void ShowGameOver(string loseReasonText)
     {
-        gameOverScreen.SetActive(true);
-        pointsText.text = "¡ " + scoreManager.currentScore.ToString() + " points !";
-        loseReason.text = loseReasonText;
-        gameOver = true;
+        StartCoroutine(ShowGameOverWithDelay(loseReasonText));
+    }
+
+    private IEnumerator ShowGameOverWithDelay(string loseReasonText)
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        if (ammoManager.GetAmmo() == 0)
+        {
+            gameOverScreen.SetActive(true);
+            pointsText.text = "¡ " + scoreManager.currentScore.ToString() + " points !";
+            loseReason.text = loseReasonText;
+            gameOver = true;
+        }
     }
 
     public void Restart()
